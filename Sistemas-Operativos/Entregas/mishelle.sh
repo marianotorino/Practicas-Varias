87def65e6f2d4cb9447fc2f80baa3cd84a0f92e7
Ramiro Antonio <ramiro.antonio@outlook.com>
2016-04-27 11:02:41 -0300

    Casi terminado

--- Sistemas-Operativos/Entregas/mishelle.sh
+++ Sistemas-Operativos/Entregas/mishelle.sh
@@ -9,15 +9,15 @@ export RUTA="$SO/externalSO;/bin;/usr/bin;/usr/local/bin"
 #Debug mode on/off
 dm=false
 if [[ $# -gt 0 && $1 == "-d" ]] ; then
-	DC='\033[1;32m'
-	NC='\033[0m'
+	DEBCOLOR='\033[1;32m'
+	NOCOLOR='\033[0m'
 	dm=true
-	echo -e ${DC}DEBUG MODE ON${NC}
+	echo -e ${DEBCOLOR}DEBUG MODE ON${NOCOLOR}
 fi
 
 function debugprint(){
 	if [ "$dm" = true ] ; then
-		echo -e "${DC}DEBUG${NC}: $1"
+		echo -e "${DEBCOLOR}DEBUG${NOCOLOR}: $1"
 	fi
 }
 
@@ -39,17 +39,25 @@ function lsfunc(){
 		for file in $(ls $reverse)
 		do
 			debugprint "Haciendo ls -l de $file"
-			data=$(ls -l $file)
+			if [[ -d $file ]] ; then			
+				data=$(ls -ld $file)
+			else
+				data=$(ls -l $file)
+			fi
 			hexperm=$(stat -c "%a" $(echo $data | cut -d" " -f9))
 			echo ${data:0:1}$hexperm${data:10}
 		done
 	else
 		for dir in ${*:2}
 		do
-			debugprint "files in $dir"
-			echo $dir:			
-			ls -1 $dir $reverse &
-			wait
+			if [[ -d $dir && -r $dir ]] ; then
+				debugprint "files in $dir"
+				echo $dir:			
+				ls -1 $dir $reverse &
+				wait
+			else
+				echo El parametro $dir no es un directorio o no posee permisos.
+			fi
 		done
 	fi
 }
@@ -84,15 +92,68 @@ quiensoyfunc(){
 	fi
 }
 
+mkdirfunc(){
+	if [ $# -gt 0 ] ; then
+		if [ ${1:0:1} == "/" ] ; then
+			debugprint "es un directorio absoluto"
+			dir=$1
+		else
+			debugprint "es un directorio relativo"
+			dir="$(pwd)/"$1
+		fi
+		debugprint "creando al directorio $dir"
+		dashp="false"
+		index=1
+		args=${@:1}
+		for param in ${@:1}
+		do
+			if [ $param == "-p" ] ; then			
+				debugprint "encontrado el parametro -p"				
+				indexp=$index
+				debugprint "argumentos: $(echo $args | tr '\n' ' ' )"
+				newargs=$(echo ${args//-p} | tr '\n' ' ')
+				debugprint "nuevos argumentos: $nweargs"				
+				dashp="true"
+				break
+			fi
+			let index++
+		done
+		if [ "$dashp" == "true" ] ; then
+			aux=$dir
+			while [ ! -e $aux ]
+			do
+				directories[]=$aux
+				aux=$(echo $aux | rev | cut -d"/" -f2- | rev)
+			done
+			for path in directories
+			do
+				mkdirfunc $path ${@:1} #pero sin -p
+			done
+		else
+			if [[ -w $(echo $dir | rev | cut -d"/" -f2- | rev) ]]
+			then
+				mkdir $dir ${@:2} 2> /dev/null
+				if [ $? -ne 0 ] ; then
+					echo No se pudo crear el directorio
+				fi
+			else
+				echo No tiene permiso de escritura en el directorio
+			fi
+		fi
+	else
+		echo Debe especificar un directorio para crear
+	fi
+}
+
 default(){
 	if [ $(type -f $1 2> /dev/null | grep "builtin" | wc -l) -eq 1 ]
 	then
 		debugprint "comando interno de bash"
-		$* 
+		command $* 
 	elif [[ ${1:0:2} == "./" && -x $1 ]]
 	then
 		debugprint "comando con ./"
-		$*
+		$* &
 	else
 		noexists=true
 		for path in $(echo $RUTA | tr ";" "\n" | tac)
@@ -101,9 +162,10 @@ default(){
 			debugprint $path/$1
 			if [ -x $path/$1 ]
 			then
-				debugprint "encontrado en la ruta $path"
+				debugprint "encontrado"
 				noexists=false
-				$path/$*
+				$path/$* &
+				wait
 				break
 			fi
 		done
@@ -125,6 +187,9 @@ while true; do
 	read -p $(echo $prompt) -a line
 
 	cmd=${line[0]}
+	if [ -z "$cmd" ] ; then
+		continue
+	fi
 	args=${line[@]:1}
 
 	debugprint "ejecutando comando: $cmd con argumentos: $args"
@@ -150,12 +215,7 @@ while true; do
 		"quiensoy" )
 		quiensoyfunc $args ;;
 		"mkdir" )
-		if [[ -w $(pwd) ]]
-		then
-			mkdir $args
-		else
-			echo No tiene permiso de escritura en el directorio actual
-		fi ;;
+		mkdirfunc $args ;;
 		* )
 		default $cmd $args
 	esac
